be0ff8ae9a5b2d15e5c35bfec16b1a8c94a8c806
Joao Ramos <joaoramos552@gmail.com>
2022-04-12 16:39:47 -0300

    Projeto criado

---
+++ src/boardgame/Board.java
@@ -0,0 +1,46 @@
+package boardgame;
+
+public class Board {
+	
+	private int rows;
+	private int colunms;
+	private Piece[][] pieces;
+	
+	public Board(int rows, int colunms) {
+		this.rows = rows;
+		this.colunms = colunms;
+		pieces = new Piece[rows][colunms];
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getColunms() {
+		return colunms;
+	}
+
+	public void setColunms(int colunms) {
+		this.colunms = colunms;
+	}
+	
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
+	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
+	
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
+	
+	
+
+}
